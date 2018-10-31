1284448578881f80b8b7afbea32a0876a73e49ff
chenliujin <liujin.chen@qq.com>
2018-10-31 18:18:23 +0800

    deal.sh

--- bin/stock/deal.sh
+++ bin/stock/deal.sh
@@ -7,7 +7,11 @@ sqoop import \
   --table deal \
   -m 1 \
   --hive-import \
-  --create-hive-table \
-  --hive-table olap_stock.deal \
+  --hive-database raw_stock \
+  --hive-table deal \
   --delete-target-dir
 
+#hive > SET hive.exec.dynamic.partition=true;  
+#hive > SET hive.exec.dynamic.partition.mode=nonstrict; 
+#hive > SET hive.exec.max.dynamic.partitions.pernode=1000;
+#hive > insert overwrite table olap_stock.deal partition(deal_date) select * from raw_stock.deal;
