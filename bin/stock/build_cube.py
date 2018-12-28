b66b614410e129f3fcf98733afe25414759fe5f0
chenliujin <liujin.chen@qq.com>
2018-12-28 19:27:20 +0800

    build_cube.py

--- bin/stock/build_cube.py
+++ bin/stock/build_cube.py
@@ -3,6 +3,7 @@ import argparse
 import requests
 import time
 import json
+import datetime
 
 # Param
 
@@ -30,7 +31,8 @@ def build_cube(cube, startTime, endTime, buildType):
 
   r = requests.put(url, headers=headers, auth=auth, data=json.dumps(data))
 
-  print('=== build cube: ' + cube + ' ' + buildType + ' ===')
+  print('=== cube ' + cube + ' ' + buildType + ' ===')
+  print(data)
   print('status code: ' + str(r.status_code))
 
 # 传入日期数组
@@ -54,16 +56,19 @@ rs = r.json()
 results = {}
 
 for input in args.inputs:
+  #t = int(time.mktime(time.strptime(input + ' 08:00:00', '%Y-%m-%d %H:%M:%S')) * 1000)
+  t = datetime.datetime.strptime(input + ' 08:00:00', '%Y-%m-%d %H:%M:%S') + datetime.timedelta(days=1)
+  t = int(time.mktime(t.timetuple()) * 1000)
+
+  if 'endTime' not in vars():
+    endTime = t;
+  elif endTime < t:
+    endTime = t;
 
   exist = 0
   startTime = 0
 
   for segment in rs[0]['segments']:
-    t = int(time.mktime(time.strptime(input + ' 08:00:00', '%Y-%m-%d %H:%M:%S')) * 1000)
-
-    if 'endTime' not in vars():
-      endTime = t;
-
     if segment['date_range_start'] <= t and t < segment['date_range_end'] :
       exist = 1
       results[segment['date_range_start']] = {
@@ -75,10 +80,6 @@ for input in args.inputs:
       startTime = segment['date_range_end']
 
   if exist == 0 : # 不存在，build 新的 segment
-
-    if startTime in results.keys() and 'endTime' in results[startTime].keys() and results[startTime]['endTime'] < t:
-      endTime = t;
-
     results[startTime] = {
         "startTime": startTime, 
         "endTime": endTime, 
