0278d1b3dfa5558f62bb44f06a78987db0cb60cc
chenliujin <liujin.chen@qq.com>
2019-02-01 08:46:25 +0800

    ods_stock_deal.sh

--- bin/ods_stock_deal.sh
+++ bin/ods_stock_deal.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sqoop import \
-  --connect jdbc:mysql://ambari-mysql.chenliujin.com/stock?tinyInt1isBit=false \
+  --connect jdbc:mysql://mysql.chenliujin.com/stock?tinyInt1isBit=false \
   --username root \
   --password chenliujin \
   --table deal \
@@ -9,7 +9,7 @@ sqoop import \
   --direct \
   --hive-import \
   --hive-overwrite \
-  --hive-database raw_stock \
+  --hive-database ods_stock \
   --hive-table deal
 
 # * select 字段需要按表定义的顺序，分区字段在最后
@@ -17,4 +17,4 @@ sqoop import \
 #hive > SET hive.exec.dynamic.partition=true;  
 #hive > SET hive.exec.dynamic.partition.mode=nonstrict; 
 #hive > SET hive.exec.max.dynamic.partitions.pernode=1000;
-#hive > insert overwrite table olap_stock.deal partition(deal_date) select deal_id, customer_id, stock_code, deal_type, price, volume, total, stamp_tax, poundage, transfer_fee, sundry_fees, amount, status, date_added, last_modified, deal_date from raw_stock.deal;
+#hive > insert overwrite table dw_stock.deal partition(deal_date) select deal_id, customer_id, stock_code, deal_type, price, volume, total, stamp_tax, poundage, transfer_fee, sundry_fees, amount, status, date_added, last_modified, deal_date from raw_stock.deal;
